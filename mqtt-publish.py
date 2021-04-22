21f202f9848342ad4e0c9af229bc1f377cbc3b32
Sagnik Mukherjee <sagnik2207@gmail.com>
2021-04-22 09:40:24 +0530

    Mqtt Publish and Subscribe

---
+++ mqtt-publish.py
@@ -0,0 +1,10 @@
+import paho.mqtt.client as mqtt
+
+sensor_data = '24.3'
+
+client = mqtt.Client()
+client.connect("mqtt.eclipseprojects.io", 1883, 60)
+
+client.publish('topic/test', sensor_data)
+
+client.disconnect()
